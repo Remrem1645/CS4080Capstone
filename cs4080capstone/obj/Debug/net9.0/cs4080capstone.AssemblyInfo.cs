7531d2417688b2ccf4178d52f44378caedf98de7
Remrem1645 <dyan@cpp.edu>
2024-11-19 15:56:26 -0800

    move imports into _imports file

--- cs4080capstone/obj/Debug/net9.0/cs4080capstone.AssemblyInfo.cs
+++ cs4080capstone/obj/Debug/net9.0/cs4080capstone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs4080capstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c37e71f5f4c622a2dbf1a38825969d11508d2b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs4080capstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs4080capstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
